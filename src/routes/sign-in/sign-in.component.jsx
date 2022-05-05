dbbd6247c4c3ed47c390c1bfc00bfae5dbbaf364
Rylee975 <98650151+RyleeFullStackDev1@users.noreply.github.com>
2022-05-05 11:34:31 -0400

    Add sign up form with custom inputs and button

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,15 +1,34 @@
-import { createUserDocumentFromAuth, signInWithGooglePopup } from '../../utils/firebase/firebase.utils'
+import { auth, createUserDocumentFromAuth, signInWithGooglePopup, signInWithGoogleRedirect } from '../../utils/firebase/firebase.utils'
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
 const SignIn = () => {
+    useEffect(() => {
+        (async function() {
+        const response = await getRedirectResult(auth);
+        if(response) {
+            const userDocRef = await createUserDocumentFromAuth(response.user);
+        }
+        console.log(response);
+    })();
+    }, []);
+
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user);
+    }
+    const logGoogleRedirect = async () => {
+        const {user} = await signInWithGoogleRedirect();
         const userDocRef = await createUserDocumentFromAuth(user);
     }
     return (
         <div>
             <h1>Sign in page</h1>
             <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-        </div>
+            <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
+            <SignUpForm />
+            </div>
     );
 };
 
